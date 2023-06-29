7c8981cb7f01cd3b39180d9673aa5ed316438190
Artem <334947@niuitmo.ru>
2023-06-29 21:23:00 +0300

    Task 3B success

--- main.cpp
+++ main.cpp
@@ -3,157 +3,75 @@
 static const int INF = INT_MAX;
 using namespace std;
 
-struct Edge {
-    int from;
-    int to;
-    int capacity;
-    int flow;
-};
-
-vector<vector<int>> graph;
-vector<Edge> edges;
-vector<bool> used;
-
-void dfs(int v) {
-    used[v] = true;
-    for (int number : graph[v]) {
-        Edge edge = edges[number];
-        if (!used[edge.to] && edge.flow < edge.capacity) {
-            dfs(edge.to);
-        }
-    }
-}
-
-void dump(bool flag, int n) {
-    if (!flag) return;
-    for (int i = 0; i < n; i++) {
-        cout << i + 1 << ": ";
-        for (int number : graph[i]) {
-            Edge edge = edges[number];
-            cout << edge.to + 1 << " c=" << edge.capacity << " f=" << edge.flow << " ; ";
-        }
-        cout << '\n';
-    }
-    cout << '\n';
-}
-
-int bfs(int vertex, int t, int n, int m) {
-    queue<int> queue_v;
-    vector<pair<int, int>> dist(n);
-    for (int i = 0; i < n; i++) {
-        dist[i].first = INF;
-        dist[i].second = INF;
-    }
-    vector<vector<int>> path(n, vector<int>{});
-    dist[vertex].first = 0;
-    dist[vertex].second = INF;
-    queue_v.push(vertex);
-    while (!queue_v.empty()) {
-        int v = queue_v.front();
-        queue_v.pop();
-        if (v == t) {
-            break;
-        }
-        for (int number : graph[v]) {
-            Edge& edge = edges[number];
-            int to = edge.to;
-            if (dist[to].first > dist[v].first + 1 && edge.flow < edge.capacity) {
-                dist[to].first = dist[v].first + 1;
-                path[to] = path[v];
-                path[to].push_back(number);
-                queue_v.push(to);
-                int diff = min(dist[v].second, edge.capacity - edge.flow);
-                dist[to].second = diff;
-            }
-        }
-    }
-    vector<int> minimal_path = path[t];
-    if (minimal_path.empty()) {
-        return 0;
-    }
-    int res = dist[t].second;
-    for (int number : minimal_path) {
-        Edge& edge = edges[number];
-        edge.flow += res;
-        int number_reversed = number > m ? number - m : number + m;
-        Edge& reversed_edge = edges[number_reversed];
-        reversed_edge.flow -= res;
-    }
-    return res;
-}
-
 int main() {
-    bool dumpFlag = false;
-    int n, m;
-    cin >> n >> m;
-    int s = 0; int t = n-1;
-    graph.assign(n, vector<int>{});
-    edges.resize(4*m);
-    for (int i = 0; i < m; i++) {
-        int from, to, capacity;
-        cin >> from >> to >> capacity;
-        from--; to--;
-        Edge edge;
-        edge.from = from;
-        edge.to = to;
-        edge.capacity = capacity;
-        edge.flow = 0;
-        Edge back_edge;
-        back_edge.from = to;
-        back_edge.to = from;
-        back_edge.capacity = capacity;
-        back_edge.flow = 0;
-        Edge reversed_edge;
-        reversed_edge.from = to;
-        reversed_edge.to = from;
-        reversed_edge.capacity = 0;
-        reversed_edge.flow = 0;
-        Edge reversed_back_edge;
-        reversed_back_edge.from = from;
-        reversed_back_edge.to = to;
-        reversed_back_edge.capacity = 0;
-        reversed_back_edge.flow = 0;
-        edges[i] = edge;
-        edges[m+i] = back_edge;
-        edges[2*m + i] = reversed_edge;
-        edges[3*m + i] = reversed_back_edge;
-        graph[from].push_back(i);
-        graph[from].push_back(3*m + i);
-        graph[to].push_back(m+i);
-        graph[to].push_back(2*m + i);
+    ifstream fin("assignment.in");
+    int n;
+    fin >> n;
+    int C[n+1][n+1];
+    for (int i = 0; i <= n; i++) {
+        C[i][0] = 0;
     }
-
-    dump(dumpFlag, n);
-
-    while(true) {
-        used.assign(n, false);
-        int res = bfs(s, t, n, 2*m);
-        if (res == 0) {
-            break;
-        }
-        dump(dumpFlag, n);
+    for (int j = 0; j <= n; j++) {
+        C[0][j] = 0;
     }
-
-    int F = 0;
-    for (int number : graph[s]) {
-        F += edges[number].flow;
-    }
-
-    used.assign(n, false);
-    dfs(s);
-
-    vector<int> ans;
-    for (int i = 0; i < m; i++) {
-        int v = edges[i].from;
-        int to = edges[i].to;
-        if ((used[v] && !used[to]) || (used[to] && !used[v])) {
-            ans.push_back(i+1);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            int x;
+            fin >> x;
+            C[i][j] = x;
         }
     }
-    cout << ans.size() << ' ' << F << '\n';
-    for (int num : ans) {
-        cout << num << ' ';
+    fin.close();
+    //-------------------------------------------------------------------------------
+
+    vector<int> pot_row(n+1);
+    vector<int> pot_col(n+1);
+    vector<int> matching(n+1);
+    vector<int> path(n+1);
+    for (int i=1; i<=n; ++i) {
+        matching[0] = i;
+        int index0 = 0;
+        vector<int> min_col(n+1, INF);
+        vector<char> used (n+1, false);
+        do {
+            used[index0] = true;
+            int current_index = matching[index0], diff = INF, index1;
+            for (int j=1; j<=n; ++j)
+                if (!used[j]) {
+                    int cur = C[current_index][j]- pot_row[current_index]- pot_col[j];
+                    if (cur < min_col[j])
+                        min_col[j] = cur, path[j] = index0;
+                    if (min_col[j] < diff)
+                        diff = min_col[j], index1 = j;
+                }
+            for (int j=0; j<=n; ++j)
+                if (used[j])
+                    pot_row[matching[j]] += diff, pot_col[j] -= diff;
+                else
+                    min_col[j] -= diff;
+            index0 = index1;
+        } while (matching[index0] != 0);
+        do {
+            int index1 = path[index0];
+            matching[index0] = matching[index1];
+            index0 = index1;
+        } while (index0 != 0);
     }
-    cout << '\n';
+    vector<int> ans(n+1);
+    for (int j = 1; j<=n; j++) {
+        ans[matching[j]] = j;
+    }
+    int cost = 0;
+    for (int i = 1; i <= n; i++) {
+        cost += C[i][ans[i]];
+    }
+
+    //-------------------------------------------------------------------------------
+    ofstream fout("assignment.out");
+    fout << cost << endl;
+    for (int i = 1; i <= n; i++) {
+        fout << i << ' ' << ans[i] << endl;
+    }
+    fout.close();
     return 0;
 }

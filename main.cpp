d2870c830ee413d71ae25717b8ead116b8bcf611
Artem <334947@niuitmo.ru>
2023-06-29 17:57:37 +0300

    Task 2A success

--- main.cpp
+++ main.cpp
@@ -1,149 +1,148 @@
 #include <bits/stdc++.h>
 
+static const int INF = INT_MAX;
 using namespace std;
 
-vector<vector<int>> g;
-vector<bool> in_match_left;
-vector<bool> in_match_right;
-vector<int> from_right_to_left_in_match;
+struct Edge {
+    int from;
+    int to;
+    int capacity;
+    int flow;
+};
+
+vector<vector<int>> graph;
+vector<Edge> edges;
 vector<bool> used;
-vector<int> ans;
 
-bool find_increasing_path(int v) {
-    used[v] = true;
-    for (int to : g[v]) {
-        if (!in_match_right[to]) {
-            // found increasing path with edge v-to
-            from_right_to_left_in_match[to] = v;
-            in_match_right[to] = true;
-            in_match_left[v] = true;
-            return true;
+void dump(bool flag, int n) {
+    if (!flag) return;
+    for (int i = 0; i < n; i++) {
+        cout << i + 1 << ": ";
+        for (int number : graph[i]) {
+            Edge edge = edges[number];
+            cout << edge.to + 1 << " c=" << edge.capacity << " f=" << edge.flow << " ; ";
         }
-        int w = from_right_to_left_in_match[to];
-        if (used[w]) {
-            continue;
-        }
-        if (find_increasing_path(from_right_to_left_in_match[to])) {
-            from_right_to_left_in_match[to] = v;
-            in_match_left[v] = true;
-            in_match_left[w] = true;
-            return true;
-        }
+        cout << '\n';
     }
-    return false;
+    cout << '\n';
 }
 
-void find_max_matching(int size_left, int size_right) {
-    in_match_left.assign(size_left, false);
-    in_match_right.assign(size_right, false);
-    from_right_to_left_in_match.assign(size_right, -1);
-    for (int i = 0; i < size_left; i++) {
-        if (in_match_left[i]) {
-            continue;
+int bfs(int vertex, int t, int n, int m) {
+    queue<int> queue_v;
+    vector<pair<int, int>> dist(n);
+    for (int i = 0; i < n; i++) {
+        dist[i].first = INF;
+        dist[i].second = INF;
+    }
+    vector<vector<int>> path(n, vector<int>{});
+    dist[vertex].first = 0;
+    dist[vertex].second = INF;
+    queue_v.push(vertex);
+    while (!queue_v.empty()) {
+        int v = queue_v.front();
+        queue_v.pop();
+        if (v == t) {
+            break;
         }
-        used.assign(size_left, false);
-        find_increasing_path(i);
-
-    }
-
-//    for (int i = 0; i < size_right; i++) {
-//        int v = from_right_to_left_in_match[i];
-//        if (v != -1) {
-//            max_matching.emplace_back(v+1, i+1);
-//        }
-//    }
-
-}
-
-vector<bool> l_visited, r_visited;
-
-void dfs(int v) {
-    if (l_visited[v]) {
-        return;
-    }
-    l_visited[v] = true;
-    for (int to : g[v]) {
-        if (in_match_right[to]) {
-            int w = from_right_to_left_in_match[to];
-            if (w != v) {
-                r_visited[to] = true;
-                dfs(w);
+        for (int number : graph[v]) {
+            Edge& edge = edges[number];
+            int to = edge.to;
+            if (dist[to].first > dist[v].first + 1 && edge.flow < edge.capacity) {
+                dist[to].first = dist[v].first + 1;
+                path[to] = path[v];
+                path[to].push_back(number);
+                queue_v.push(to);
+                int diff = min(dist[v].second, edge.capacity - edge.flow);
+                dist[to].second = diff;
             }
-        } else {
-            r_visited[to] = true;
         }
     }
-}
-
-void process_request(int n) {
-
-    find_max_matching(n, n);
-    l_visited.assign(n, false);
-    r_visited.assign(n, false);
-    for (int i = 0; i < n; i++) {
-        if (!in_match_left[i]) {
-            dfs(i);
-        }
+    vector<int> minimal_path = path[t];
+    if (minimal_path.empty()) {
+        return 0;
     }
-    // MVC = l- and r+
-    // В ответ те вершины, которые не в l- и не в r+ то есть
-    for (int i = 0; i < n; i++) {
-        if (l_visited[i] && !(r_visited[i])) {
-            ans.push_back(i);
-        }
+    int res = dist[t].second;
+    for (int number : minimal_path) {
+        Edge& edge = edges[number];
+        edge.flow += res;
+        int number_reversed = number > m ? number - m : number + m;
+        Edge& reversed_edge = edges[number_reversed];
+        reversed_edge.flow -= res;
     }
+    return res;
 }
 
 int main() {
-    ifstream fin("hobbits.in");
-    int n;
-    fin >> n;
-    vector<vector<int>> matrix(n, vector<int>{});
-    for (int i = 0; i < n; i++) {
-        int a;
-        for (int j = 0; j < n; j++) {
-            fin >> a;
-            matrix[i].push_back(a);
+    bool dumpFlag = false;
+    int n, m;
+    cin >> n >> m;
+    int s = 0; int t = n-1;
+    graph.assign(n, vector<int>{});
+    edges.resize(4*m);
+    for (int i = 0; i < m; i++) {
+        int from, to, capacity;
+        cin >> from >> to >> capacity;
+        from--; to--;
+        Edge edge;
+        edge.from = from;
+        edge.to = to;
+        edge.capacity = capacity;
+        edge.flow = 0;
+        Edge back_edge;
+        back_edge.from = to;
+        back_edge.to = from;
+        back_edge.capacity = capacity;
+        back_edge.flow = 0;
+        Edge reversed_edge;
+        reversed_edge.from = to;
+        reversed_edge.to = from;
+        reversed_edge.capacity = 0;
+        reversed_edge.flow = 0;
+        Edge reversed_back_edge;
+        reversed_back_edge.from = from;
+        reversed_back_edge.to = to;
+        reversed_back_edge.capacity = 0;
+        reversed_back_edge.flow = 0;
+        edges[i] = edge;
+        edges[m+i] = back_edge;
+        edges[2*m + i] = reversed_edge;
+        edges[3*m + i] = reversed_back_edge;
+        graph[from].push_back(i);
+        graph[from].push_back(3*m + i);
+        graph[to].push_back(m+i);
+        graph[to].push_back(2*m + i);
+    }
+
+    dump(dumpFlag, n);
+
+    while(true) {
+        used.assign(n, false);
+        int res = bfs(s, t, n, 2*m);
+        if (res == 0) {
+            break;
+        }
+        dump(dumpFlag, n);
+    }
+
+    int F = 0;
+    for (int number : graph[s]) {
+        F += edges[number].flow;
+    }
+    cout << F << '\n';
+
+    for (int i = 0; i < m; i++) {
+        Edge edge = edges[i];
+        Edge back_edge = edges[m+i];
+        int f_edge = edge.flow;
+        int f_rev = back_edge.flow;
+        if (f_edge != 0) {
+            cout << f_edge << '\n';
+        } else if (f_rev != 0) {
+            cout << -f_rev << '\n';
+        } else {
+            cout << 0 << '\n';
         }
     }
-    fin.close();
 
-    // транзитивное замыкание графа
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][j] = matrix[i][k] * matrix[k][j];
-                }
-            }
-        }
-    }
-
-    g.assign(n, vector<int>{});
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] == 1) {
-                g[i].push_back(j);
-            }
-        }
-    }
-
-//    for (int i = 0; i < n; i++) {
-//        cout << i + 1 << ": ";
-//        for (int v: g[i]) {
-//            cout << v + 1 << ' ';
-//        }
-//        cout << '\n';
-//    }
-
-    process_request(n);
-
-    ofstream fout("hobbits.out");
-    fout << ans.size() << '\n';
-    for (int v : ans) {
-        fout << v+1 << ' ';
-    }
-    fout << '\n';
-    fout.close();
     return 0;
 }
